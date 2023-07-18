36f0577fcac4bb4ab9768d0c2fdec51e94680a1f
Brandon Awan <abibangbrandon855@gmail.com>
2023-07-18 09:49:52 +0100

    fixes to responsivenes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const XLSX = require('xlsx');
+const { validationResult } = require('express-validator');
+
 const ejs = require("ejs");
 const app = express();
 // Start the server
@@ -18,7 +21,7 @@ app.use(express.static("public"));
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_LOCAL_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -30,7 +33,7 @@ mongoose
   });
 
 // connect to emailSchema.js
-// const Email = require('./models/emailSchema');
+const Email = require('./models/emailSchema');
 
 
 // Middleware
@@ -133,6 +136,50 @@ app.post('/send-email', (req, res) => {
   });
 
 
+  // Handle newsletter submission
+  app.post('/notify', async (req, res) => {
+    try {
+      const { email } = req.body;
+      if (!email) {
+        return res.render('index', { message: null, error: 'Email is required' });
+      }
+  
+      const existingEmail = await Email.findOne({ email });
+      if (existingEmail) {
+        return res.render('index', { message: 'Email already submitted', error: null });
+      }
+  
+      const userEmail = await Email.create({ email });
+  
+      let workbook;
+      try {
+        workbook = XLSX.readFile('emails.xlsx');
+      } catch (error) {
+        // File doesn't exist, create a new workbook with a sheet
+        workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, XLSX.utils.aoa_to_sheet([]), 'Emails');
+      }
+  
+      const worksheet = workbook.Sheets['Emails'];
+  
+      const lastRow = XLSX.utils.sheet_add_json(worksheet, [{ email }], { skipHeader: true, origin: -1 });
+  
+      XLSX.writeFile(workbook, 'emails.xlsx');
+  
+      return res.render('success', { message: 'Thank you for subscribing. We\'ll notify you when we launch!' });
+    } catch (error) {
+      console.error(error);
+      return res.render('error', { message: null, error: 'Oops! Something went wrong. Please try again later.' });
+    }
+  });   
+
+
+  // Endpoint to download the Excel file
+app.get('/download-emails', (req, res) => {
+    res.download('emails.xlsx', 'emails.xlsx');
+});
+
+    
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
